0ead7a2e12fbbaa6c8ab35b4b810421d36c72e95
GameDevBill <39572846+gamedevbill@users.noreply.github.com>
2020-08-04 19:07:21 -0500

    adding a URP version of normal calculation graphs

---
+++ NormalCalc_URP/Assets/Scripts/rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour
+{
+    public Vector3 factor;
+    Transform t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //t.Rotate(0, 0.5f, 0);
+        t.Rotate(factor);
+    }
+}
